fd39241b758375516b8f06522fc652c81bfa5d16
Agustin Becerra <beche29@gmail.com>
2016-10-20 09:27:30 -0300

    Menu lateral funcionando

--- app/src/main/java/yuber/yuber/activity/MapActivity.java
+++ app/src/main/java/yuber/yuber/activity/MapActivity.java
@@ -1,16 +1,20 @@
 package yuber.yuber.activity;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import yuber.yuber.R;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener  {
 
     private Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,10 @@ public class MapActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
 
     }
 
@@ -49,4 +56,7 @@ public class MapActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+    }
 }
